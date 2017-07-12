fd87ac0e410f67b12e772330f07b0b8aa06a70f8
Mikhail Bocharov <mike.b.89@mail.ru>
2017-07-12 19:17:25 +0300

    2dn commit - index.php start

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 12.07.17
+ * Time: 19:14
+ *
+ *  * code sniffer enable
+ * code style PSR2
+ * emmet plugin and gitignore active
+ *
+ * every task i will require to this file
+ */
