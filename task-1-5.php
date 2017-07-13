2f35b08c454f13d7255a5c12899b3fdb4703d8e1
Mikhail Bocharov <mike.b.89@mail.ru>
2017-07-13 16:41:07 +0300

    END of 1.5 task

---
+++ task-1-5.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 13.07.17
+ * Time: 15:32
+ */
+
+$day = mt_rand(0, 10 );
+
+switch ($day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo "Это рабочий день"."<br/>";
+        break;
+    case 6:
+    case 7:
+        echo "Это выходной день"."<br/>";
+        break;
+    default:
+        echo "Это неизвестный день"."<br/>";
+}
