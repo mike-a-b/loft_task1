15a0c9f13bf5cbe6c8a90819d674d2d2762bb76b
Mikhail Bocharov <mike.b.89@mail.ru>
2017-07-15 11:13:44 +0300

    revision task 1-7 (add simple nunmbers)

--- task-1-7.php
+++ task-1-7.php
@@ -6,7 +6,7 @@
  * Time: 18:46
  */
 
-echo "<h2>Задание # 1.7 v.2</h2>";
+echo "<h2>Задание # 1.7 v.3</h2>";
 
 echo "<table style='border:1px solid black; border-collapse: collapse;'>";
 for ($v1 = 1; $v1 <= 10; $v1++) {
@@ -14,8 +14,10 @@ for ($v1 = 1; $v1 <= 10; $v1++) {
     for ($v2 = 1; $v2 <= 10; $v2++) {
         if (($v1 % 2 === 0) && ($v2 % 2 === 0)) {
             echo "<td style='border:1px solid black;'>&nbsp[" . $v1 * $v2 . "]&nbsp</td>";
-        } else {
+        } elseif (($v1 % 2 !== 0) && ($v2 % 2 !== 0)) {
             echo "<td style='border:1px solid black;'>&nbsp(" . $v1 * $v2 . ")&nbsp</td>";
+        } else {
+            echo "<td style='border:1px solid black;'>&nbsp" . $v1 * $v2 . "&nbsp</td>";
         }
     }
     echo "</tr>";
